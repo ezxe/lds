449c8869d85647742a6fee8a8fc02d2a7d1e8deb
eze <fw.soporte16@gmail.com>
2019-03-15 17:12:28 -0300

    actualizado commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif -->
                     @if(Auth::user()->hasRole('admin'))
-                    <div>Acceso como administrador</div>
+                    <div>Acceso como administrador1</div>
                          @else
                     <div>Acceso usuario</div>
                      @endif
